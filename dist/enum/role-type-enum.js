3708f284365467d152479370bf8323b69ba529b6
Пасічна Ярина <pas06chna@gmail.com>
2025-01-29 03:08:34 +0200

    6

--- dist/enum/role-type-enum.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RoleTypeEnum = void 0;
-var RoleTypeEnum;
-(function (RoleTypeEnum) {
-    RoleTypeEnum["Admin"] = "admin";
-    RoleTypeEnum["User"] = "user";
-})(RoleTypeEnum || (exports.RoleTypeEnum = RoleTypeEnum = {}));
